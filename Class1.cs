dda1111fb6f1f6621fb92c61ad0e136c80fd339f
Shubham Kumar <127440557+Imshubhham@users.noreply.github.com>
2025-08-01 14:29:14 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using RGiesecke.DllExport;
+
+[ComVisible(true)]
+public class MyExports
+{
+    [DllExport("Add", CallingConvention = CallingConvention.Cdecl)]
+    public static void Add(double a, double b, out double c)
+    {
+        c = a + b;
+    }
+}
